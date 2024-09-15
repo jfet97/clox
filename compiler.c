e7f7791d42d028e01c1e47e08a9a41c7441613cb
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-09-15 16:31:47 +0200

    first steps hacking the compiler

--- compiler.c
+++ compiler.c
@@ -1,23 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-  initScanner(source);
-  int line = -1;
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAtCurrent(const char* message) {
+  errorAt(&parser.current, message);
+}
+
+static void error(const char* message) {
+  errorAt(&parser.previous, message);
+}
+
+static void errorAt(Token* token, const char* message) {
+  // suppress errors after the first one (will be cleared at synchronization points)
+  if(parser.panicMode) return;
+  parser.panicMode = true;
+
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if(token->type == TOKEN_EOF) {
+    fprintf(stderr, " at end");
+  } else if(token->type == TOKEN_ERROR) {
+    // Nothing
+  } else {
+    fprintf(stderr, "at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void advance() {
+  parser.previous = parser.current;
 
   while(true) {
-    Token token = scanToken();
-    if(token.line != line) {
-      printf("%4d ", token.line);
-      line = token.line;
-    } else {
-      printf("   | ");
-    }
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+    parser.current = scanToken();
+    if(parser.current.type != TOKEN_ERROR) break;
 
-    if(token.type == TOKEN_EOF) break;
+    // the scanner doesn't report lexical errors, it creates error tokens
+    // and leaves it up to the parser to report them
+    errorAtCurrent(parser.current.start);
   }
+}
+
+static void consume(TokenType type, const char* message) {
+  // check if the current token is of the expected type before advancing
+  if(parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+bool compile(const char* source, Chunk* chunk) {
+  initScanner(source);
+
+  parser.hadError = false;
+  parser.panicMode = false;
+
+
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expect end of expression.");
+  return !parser.hadError;
 }
